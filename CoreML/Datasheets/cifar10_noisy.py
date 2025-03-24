304560e6df9d2398ce47a978f76f18f170e0f2c6
Madhvansh <choksimac167005@gmail.com>
2025-03-24 20:40:36 +0530

    Datasheet Folder initiation

---
+++ CoreML/Datasheets/cifar10_noisy.py
@@ -0,0 +1,9 @@
+import torch
+from torchvision import datasets, transforms
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set random seed for reproducibility
+torch.manual_seed(42)
+np.random.seed(42)
+
